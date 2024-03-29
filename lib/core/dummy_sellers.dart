034141a4b8123f3695195e24cb017dea49bfd698
h4fef <afef.hassani@fiven.eu>
2024-03-29 14:38:44 +0100

    descrizioni prodotti

--- lib/core/dummy_sellers.dart
+++ lib/core/dummy_sellers.dart
@@ -29,7 +29,8 @@ List<Seller> sellers = [
         category: categories[2].name.toString(),
         minutes: 5,
         image: Assets.birraTuborg,
-        description: "Una corona buonissima!",
+        description:
+            "Tuborg, la birra danese dall'inconfondibile gusto fresco e bilanciato. Con la sua tradizione secolare di qualità e innovazione, Tuborg offre un'esperienza di bevuta rinfrescante e appagante. Gusta la sua leggerezza e la sua finezza in ogni sorso, perfetta per accompagnare i momenti conviviali e le serate speciali. Scopri il piacere della birra Tuborg e immergiti in un'esperienza unica di gusto nordico.",
         price: oCcy.format(5.00),
       ),
       Product(
@@ -38,7 +39,8 @@ List<Seller> sellers = [
         category: categories[2].name.toString(),
         minutes: 5,
         image: Assets.birraPeroniGran,
-        description: "Una corona buonissima!",
+        description:
+            "Peroni è l'epitome dell'eleganza italiana nel mondo della birra. Questa lager premium, dal carattere distintivo e dal gusto fresco e bilanciato, rappresenta l'eccellenza artigianale italiana da oltre 150 anni. Con la sua schiuma dorata e il suo aroma invitante, ogni sorso di Peroni è un viaggio sensoriale attraverso la tradizione e la qualità che solo l'Italia sa offrire",
         price: oCcy.format(5.00),
       ),
       Product(
@@ -47,7 +49,8 @@ List<Seller> sellers = [
         category: categories[2].name.toString(),
         minutes: 5,
         image: Assets.nastro,
-        description: "Una corona buonissima!",
+        description:
+            "Birra Nastro Azzurro: una lager premium dall'Italia, caratterizzata da un gusto fresco e leggero con note di malto e luppolo. La sua schiuma bianca e persistente e il colore dorato la rendono un'icona di stile e qualità. Perfetta da gustare in compagnia o per celebrare momenti speciali, è la scelta ideale per chi cerca un'esperienza di birra raffinata e soddisfacente.",
         price: oCcy.format(5.00),
       ),
       Product(
@@ -56,7 +59,8 @@ List<Seller> sellers = [
         category: categories[2].name.toString(),
         minutes: 5,
         image: Assets.peroniRossa,
-        description: "Una corona buonissima!",
+        description:
+            "Peroni Gran Riserva Rossa è una birra dal carattere distintivo, ricca e avvolgente. Con il suo colore ambrato e la schiuma persistente, offre un bouquet aromatico complesso con note di malto tostato e caramello. Il suo gusto rotondo e bilanciato, arricchito da sentori di frutta secca e una leggera speziatura, la rende l'ideale compagna per momenti di relax e convivialità",
         price: oCcy.format(5.00),
       ),
     ],
@@ -77,7 +81,8 @@ List<Seller> sellers = [
         category: categories[4].name.toString(),
         minutes: 15,
         image: Assets.soda,
-        description: "Una soda buonissima!",
+        description:
+            "L'Oran Soda è una bibita gassata dalla dolcezza equilibrata e dal caratteristico sapore di arancia. Fresca e vivace, questa bevanda rinfrescante è perfetta per soddisfare la sete in qualsiasi momento della giornata. Con il suo gusto fruttato e la leggera effervescenza, l'Oran Soda è un'opzione deliziosa per chi cerca un'alternativa alle bevande tradizionali.",
         price: oCcy.format(3.00),
       ),
       Product(
@@ -86,7 +91,8 @@ List<Seller> sellers = [
         category: categories[4].name.toString(),
         minutes: 15,
         image: Assets.cocaCola,
-        description: "Una coca cola buonissima!",
+        description:
+            "Coca-Cola: la bevanda iconica che ha conquistato il mondo con il suo gusto unico e rinfrescante. Con la sua ricetta segreta e la bolla perfetta, Coca-Cola porta gioia e felicità ad ogni sorso. Perfetta da sola o come accompagnamento a qualsiasi pasto, è la scelta preferita di milioni di persone in tutto il mondo. Scopri il piacere autentico di Coca-Cola e vivi un momento di freschezza e soddisfazione.",
         price: oCcy.format(3.00),
       ),
       Product(
@@ -95,7 +101,8 @@ List<Seller> sellers = [
         category: categories[4].name.toString(),
         minutes: 15,
         image: Assets.sprite,
-        description: "Una sprite buonissima!",
+        description:
+            "Sprite, la bevanda rinfrescante senza zucchero, con il suo gusto unico di limone e lime, ti regala una fresca esplosione di energia in ogni sorso. Con la sua formula leggera e frizzante, Sprite è la scelta perfetta per rinfrescarti in ogni momento della giornata, aggiungendo una spruzzata di vivacità al tuo mondo.",
         price: oCcy.format(3.00),
       ),
     ],
@@ -117,7 +124,8 @@ List<Seller> sellers = [
         category: categories[3].name.toString(),
         minutes: 10,
         image: Assets.berryTea,
-        description: "description",
+        description:
+            "Scopri il nostro delizioso bubble tea al gusto di bacche! Un'esplosione di freschezza e fruttuosità ti attende in ogni sorso. Gusta la dolcezza naturale delle bacche bilanciata dalla cremosità del tè, arricchita dalle morbide perle di tapioca. Un'esperienza frizzante e vibrante che ti porterà in un mondo di sapore e piacere!",
         price: oCcy.format(8.00),
       ),
       Product(
@@ -126,7 +134,8 @@ List<Seller> sellers = [
         category: categories[3].name.toString(),
         minutes: 10,
         image: Assets.peachTea,
-        description: "description",
+        description:
+            "Esplora la freschezza del nostro bubble tea al gusto di pesca! Deliziati con un'esplosione di dolcezza e un tocco di frizzantezza in ogni sorso. Gusta la dolcezza naturale della pesca unita alla morbidezza delle perle di tapioca per un'esperienza irresistibile e rinfrescante. Scopri il lato più fruttato del bubble tea nel nostro locale, dove il gusto della pesca incontra la gioia delle bolle!",
         price: oCcy.format(8.00),
       ),
     ],
@@ -148,7 +157,8 @@ List<Seller> sellers = [
         category: categories[1].name,
         minutes: 2,
         image: Assets.cappuccino,
-        description: "descrizione",
+        description:
+            "Un'iconica bevanda italiana, il cappuccino è un equilibrio perfetto tra espresso intenso e latte cremoso, coronato da una soffice schiuma. Il suo gusto rotondo e avvolgente è arricchito dalla dolcezza naturale del latte, mentre la schiuma aggiunge una nota vellutata alla superficie. Un'esperienza di puro piacere per gli amanti del caffè, perfetta per iniziare la giornata con energia o concedersi un momento di relax",
         price: oCcy.format(2.00),
       ),
     ],

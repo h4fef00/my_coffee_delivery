66c081b970dbaa6d9c6ffa7a420c873dc91d2079
h4fef <afef.hassani@fiven.eu>
2024-03-29 14:33:20 +0100

    sistemato risoluzione (altezza) immagini visualizzate del prodotto singolo + eliminato file iniziale della lista prodotti

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_coffee_delivery/core/dummy_products.dart';
 import 'package:my_coffee_delivery/core/dummy_sellers.dart';
-import 'package:my_coffee_delivery/models/category.dart';
-import 'package:my_coffee_delivery/models/product.dart';
 import 'package:my_coffee_delivery/models/seller.dart';
 import 'package:my_coffee_delivery/views/home/seller_list.dart';
 import 'package:my_coffee_delivery/views/widgets/search_bar.dart';
@@ -16,24 +13,7 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   String? cat;
-  List<Product> filteredProducts = dummyProducts;
   List<Seller> sellersList = sellers;
-
-  @override
-  void initState() {
-    super.initState();
-    _filterProductsByCat(null);
-  }
-
-  void _filterProductsByCat(Category? cat) {
-    setState(() {
-      if (cat != null) {
-        filteredProducts = dummyProducts
-            .where((product) => product.category == cat.name)
-            .toList();
-      }
-    });
-  }
 
   @override
   Widget build(context) {

a709d61021d9ed05c60b9d8719800619085ff149
h4fef <afef.hassani@fiven.eu>
2024-04-02 11:59:35 +0200

    aggiunto quantità e icona categoria

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,17 +1,21 @@
 class Product {
-  Product(
-      {required this.id,
-      required this.name,
-      required this.category,
-      required this.image,
-      required this.description,
-      required this.price});
+  Product({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.categoryIcon,
+    required this.image,
+    required this.description,
+    required this.price,
+    required this.quantity,
+  });
 
   final String id;
   final String name;
   final String price;
   final String category;
-
+  final String categoryIcon;
   final String image;
   final String description;
+  int quantity;
 }

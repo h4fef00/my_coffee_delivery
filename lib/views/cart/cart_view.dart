bd65ed7fb3cb3d88f8782f944fca9819804a9a86
h4fef <afef.hassani@fiven.eu>
2024-04-04 16:28:19 +0200

    layout carrello

--- lib/views/cart/cart_view.dart
+++ lib/views/cart/cart_view.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_coffee_delivery/models/product.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -34,7 +34,6 @@ class _CartViewState extends State<CartView> {
         },
       );
       print(prefsData.length);
-      // print(favorites.toString());
     } else {
       print("Nessun oggetto");
     }
@@ -44,7 +43,7 @@ class _CartViewState extends State<CartView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Carrello"),
+        title: const Text('Carrello'),
         automaticallyImplyLeading: false,
       ),
       // testo per carrello vuoto
@@ -52,45 +51,257 @@ class _CartViewState extends State<CartView> {
         child: Text("Non hai nessun prodotto nel carrello"),
         // aggiungere visualizzazione se carrello pieno
       ),*/
-          ListView.builder(
-        itemCount: cart.length,
-        itemBuilder: (context, index) {
-          var prefs = cart[index];
-          return Card(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              margin: const EdgeInsets.symmetric(vertical: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
+          Container(
+        padding: const EdgeInsets.all(8),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                child: Text(
+                  "Il tuo ordine",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                child: Text.rich(
+                  TextSpan(
                     children: [
-                      SizedBox(
-                        width: 65,
-                        child: Image.asset(prefs.image),
+                      TextSpan(text: '${cart.length.toString()} prodotto da '),
+                      const WidgetSpan(
+                        child: Text(
+                          "NomeVenditore",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ],
                   ),
-                  Text(
-                    prefs.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      // _addToFavorites(widget.product);
-                    },
-                    icon: prefs.isFavorite
-                        ? const Icon(Icons.favorite)
-                        : const Icon(Icons.favorite_border),
-                  ),
-                ],
+                ),
               ),
-            ),
-          );
-        },
+              const SizedBox(
+                height: 10,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: cart.length,
+                itemBuilder: (context, index) {
+                  var prod = cart[index];
+                  // var seller = sellers[index].products; mostrare nome seller
+                  return ListTile(
+                    leading: Text(
+                      '${prod.quantity.toString()}x',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                    title: Text(
+                      prod.name,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    subtitle: const Text(
+                      "Nome venditore",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 122, 122, 122),
+                      ),
+                    ),
+                    // prezzo dev'essere x quantità, al momento non c'è nessun calcolo
+                    trailing: Text(
+                      prod.price,
+                      style: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                child: Text(
+                  "Dettagli consegna",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const ListTile(
+                leading: Icon(Icons.pin_drop, color: Colors.brown),
+                title: Text(
+                  "Via indirizzo finto, 1",
+                  style: TextStyle(fontSize: 15),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Color.fromARGB(255, 122, 122, 122),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.brown.shade500,
+                            ),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            color: Colors.brown.shade200,
+                          ),
+                          child: const Column(
+                            children: [
+                              Text(
+                                // aggiungere minuti dinamici
+                                '0 min',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Appena possibile",
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.brown.shade100,
+                            ),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            color: Colors.brown.shade50,
+                          ),
+                          child: const Column(
+                            children: [
+                              Text(
+                                // aggiungere ontap con funzione che apre ruota con data e orario
+                                'Programma ordine',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Scegli data ed orario",
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                child: Text(
+                  "Riepilogo",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Prodotto"),
+                    Text("Prezzo"),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Consegna"),
+                    Text("1.99"),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(text: 'Spese servizio '),
+                          // icona deve aprire finestra basso schermo
+                          WidgetSpan(
+                            child: Icon(
+                              Icons.info,
+                              size: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text("0.20"),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "TOTALE",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      "0.00",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+              // bottone paga che mostra pagina di conferma ordine x 1 secondo
+              // svuotare carrello (local storage) e array
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                decoration: const BoxDecoration(
+                  color: Colors.brown,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 120.0, vertical: 20),
+                  child: Text(
+                    "Conferma ordine",
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

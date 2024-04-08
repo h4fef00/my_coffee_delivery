0b8818bb3dacdf4ec45706a2fcb0b9b5aeb2f42b
h4fef <afef.hassani@fiven.eu>
2024-04-08 10:28:01 +0200

    provider come wrapper dell'app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:my_coffee_delivery/provider/cart_provider.dart';
 import 'package:my_coffee_delivery/views/on_boarding/on_boarding_view.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => CartProvider()),
+    ],
+    child: const MaterialApp(
       title: "My coffee delivery",
       home: OnBoardingView(),
     ),
-  );
+  ));
 }
